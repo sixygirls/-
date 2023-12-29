6f8b830ffef4f1ffe0adf3d157cf5c268522bb60
sixygirls <155191734+sixygirls@users.noreply.github.com>
2023-12-29 21:47:01 +0200

    Update save_location.php

--- save_location.php
+++ save_location.php
@@ -1,8 +1,8 @@
 <?php 
 &my_file = fopen("location.txt", "w");
 &information = "lat:" . &GET["lat"] . "\nlong:" .&GET["long"];
-fwrite(&my_file, &information)
-fclose(&my_file)
+fwrite(&my_file, &information);
+fclose(&my_file);
 
 
 ?>
